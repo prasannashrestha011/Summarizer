e8add5675208e1dedf1435153a5446aadc29daf0
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-03-09 21:30:27 +0545

    service for history list

--- server/app/Models/HistoryModel.py
+++ server/app/Models/HistoryModel.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import Optional
+import uuid
 
 from pydantic import BaseModel
 from sqlmodel import Field, Relationship, SQLModel
@@ -7,21 +8,23 @@ from sqlmodel import Field, Relationship, SQLModel
 
 class HistoryModel(SQLModel, table=True):
     __tablename__ = "user_history"
-    chat_id: str = Field(primary_key=True)
+    chat_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     user_id: str
-    messages: list["ConversationMessage"] = Relationship(back_populates="chat")
+    messages: Optional[list["ConversationMessage"]] = Relationship(
+        back_populates="chat"
+    )
 
 
 class ConversationMessage(SQLModel, table=True):
     __tablename__ = "converstaion_messages"
-
+    conv_id: uuid.UUID = Field(primary_key=True, default_factory=uuid.uuid4)
     role: str
     message: str
     timestamp: datetime = Field(default_factory=lambda: datetime.now())
-    chat_id: str = Field(primary_key=True, foreign_key="user_history.chat_id")
+    chat_id: uuid.UUID = Field(foreign_key="user_history.chat_id")
     chat: Optional[HistoryModel] = Relationship(back_populates="messages")
 
 
 class HistoryResponseModel(BaseModel):
     user_id: str
-    messages: list["ConversationMessage"]
+    messages: list["ConversationMessage"] = []
--- server/app/services/HistoryService.py
+++ server/app/services/HistoryService.py
@@ -1,8 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from Models.HistoryModel import HistoryResponseModel
+from Models.HistoryModel import HistoryModel, HistoryResponseModel
 
 
 class HistoryService:
     def __init__(self, db: AsyncSession):
         self.db = db
+
+    async def create_new_chat(self, user_id: str):
+        async with self.db.begin():
+            history_model = HistoryModel(user_id=user_id)
+            self.db.add(history_model)
